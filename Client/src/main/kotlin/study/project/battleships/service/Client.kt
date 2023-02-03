d70db644d05fc07623cf11e5b4202e606b3b3640
kino <joaquin.ayllongar@gmail.com>
2023-02-02 22:27:35 -0800

    views

--- Client/src/main/kotlin/study/project/battleships/service/Client.kt
+++ Client/src/main/kotlin/study/project/battleships/service/Client.kt
@@ -1,14 +1,25 @@
 package study.project.battleships.service
 
+import java.io.DataInputStream
 import java.io.InputStream
+import java.io.ObjectOutputStream
 import java.io.OutputStream
+import java.io.PrintWriter
 import java.net.Socket
 import java.util.*
 
-class Client {
+class Client(
+    socket: Socket
+) : Thread() {
+
     val id = UUID.randomUUID()
     var name = "User"
-    lateinit var socket: Socket
-    lateinit var inputStream: InputStream
-    lateinit var outputStream: OutputStream
+    val sc = Scanner(System.`in`)
+    val inputStream: InputStream = socket.getInputStream()
+    val outputStream: OutputStream = socket.getOutputStream()
+    val dataInputStream: DataInputStream = DataInputStream(inputStream)
+    val dataOutputStream: PrintWriter = PrintWriter(outputStream, true)
+    val objecOutputStream: OutputStream = ObjectOutputStream(outputStream)
+    
+    
 }
